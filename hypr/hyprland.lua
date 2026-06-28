fca8508eca605acf7c8a30efffe588f582ee854f
jonash07 <jonash07@proton.me>
2026-06-28 16:57:17 +0200

    adjusts hyprland for server

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -55,22 +55,9 @@ hl.gesture({
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
-    name        = "epic-mouse-v1",
+    name        = "steelseries-steelseries-rival-3-gen-2",
     sensitivity = -0.5,
 })
-
-
-------------------
----- MONITORS ----
-------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
--- hl.monitor({
---     output   = "",
---     mode     = "preferred",
---     position = "auto",
---     scale    = "auto",
--- })
 
 
 ---------------------
@@ -260,47 +247,11 @@ hl.config({
 
 hl.config({
     misc = {
-        force_default_wallpaper = 1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
         middle_click_paste      = false
     },
 })
-
-
----------------
----- INPUT ----
----------------
- 
--- hl.config({
---     input = {
---         kb_layout  = "us",
---         kb_variant = "",
---         kb_model   = "",
---         kb_options = "",
---         kb_rules   = "",
--- 
---         follow_mouse = 1,
--- 
---         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
--- 
---         touchpad = {
---             natural_scroll = false,
---         },
---     },
--- })
-
--- hl.gesture({
---     fingers = 3,
---     direction = "horizontal",
---     action = "workspace"
--- })
-
--- Example per-device config
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
--- hl.device({
---     name        = "epic-mouse-v1",
---     sensitivity = -0.5,
--- })
 
 
 ---------------------
